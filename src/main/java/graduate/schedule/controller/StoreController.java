2fef0f9c43ff3b67e4a18748a660af3b007bb629
ro-el-c <chung3286@naver.com>
2024-05-02 17:53:56 +0900

    feat: 가게 생성 api 구현

--- src/main/java/graduate/schedule/controller/StoreController.java
+++ src/main/java/graduate/schedule/controller/StoreController.java
@@ -1,10 +1,13 @@
 package graduate.schedule.controller;
 
+import graduate.schedule.common.response.BaseResponse;
+import graduate.schedule.dto.web.response.CreateStoreRequestDTO;
+import graduate.schedule.dto.web.response.CreateStoreResponseDTO;
 import graduate.schedule.service.StoreService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -12,4 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("store")
 public class StoreController {
     private final StoreService storeService;
+
+    /**
+     * @apiNote 가게 생성 api
+     * */
+    @PostMapping
+    public BaseResponse<CreateStoreResponseDTO> createStore(@RequestBody @Valid CreateStoreRequestDTO storeRequest) {
+        CreateStoreResponseDTO response = storeService.createStore(storeRequest);
+        return new BaseResponse<>(response);
+    }
 }
--- src/main/java/graduate/schedule/repository/StoreRepository.java
+++ src/main/java/graduate/schedule/repository/StoreRepository.java
@@ -2,8 +2,15 @@ package graduate.schedule.repository;
 
 import graduate.schedule.domain.store.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StoreRepository  extends JpaRepository<Store, Long> {
+    @Query("select case " +
+            "when count(s)> 0 then true " +
+            "else false end " +
+            "from Store s where s.inviteCode=:inviteCode")
+    boolean existsInviteCode(@Param("inviteCode") String inviteCode);
 }
