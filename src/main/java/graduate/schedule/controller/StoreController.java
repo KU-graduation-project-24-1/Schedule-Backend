32886fb17c230782b06bce139258ac0823872890
ro-el-c <chung3286@naver.com>
2024-05-02 17:17:03 +0900

    feat: create StoreController

---
+++ src/main/java/graduate/schedule/controller/StoreController.java
@@ -0,0 +1,15 @@
+package graduate.schedule.controller;
+
+import graduate.schedule.service.StoreService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("store")
+public class StoreController {
+    private final StoreService storeService;
+}
