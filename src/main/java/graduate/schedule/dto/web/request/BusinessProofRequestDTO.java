b8a403600f9515ae43c8fcf0b9303900dbf7c62d
ro-el <chung3286@naver.com>
2024-05-06 01:38:55 +0900

    feat: 가게 존재 여부 확인 및 사업자 증빙 Controller, DTO추가

---
+++ src/main/java/graduate/schedule/dto/web/request/BusinessProofRequestDTO.java
@@ -0,0 +1,11 @@
+package graduate.schedule.dto.web.request;
+
+import lombok.Getter;
+
+@Getter
+public class BusinessProofRequestDTO {
+    private String businessRegistrationNumber; //암호화 된 사업자등록번호
+    private String ceoName;
+    private String openingDate; //yyyyMMdd
+
+}
