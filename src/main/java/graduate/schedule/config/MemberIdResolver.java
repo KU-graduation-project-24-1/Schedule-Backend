da1e8a84a31eafcde8eddb9ed16c878ea97f6a49
ro-el <chung3286@naver.com>
2024-05-14 18:46:27 +0900

    refactor: memberIdResolver에서 Member 반환하도록 수정

--- src/main/java/graduate/schedule/config/MemberIdResolver.java
+++ src/main/java/graduate/schedule/config/MemberIdResolver.java
@@ -1,7 +1,12 @@
 package graduate.schedule.config;
 
 import graduate.schedule.annotation.MemberId;
+import graduate.schedule.common.exception.MemberException;
+import graduate.schedule.domain.member.Member;
+import graduate.schedule.repository.MemberRepository;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -9,8 +14,13 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+import static graduate.schedule.common.response.status.BaseExceptionResponseStatus.NOT_FOUND_MEMBER;
+
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class MemberIdResolver implements HandlerMethodArgumentResolver {
+    private final MemberRepository memberRepository;
     //Interceptor 이후 동작
 
     @Override
@@ -19,9 +29,10 @@ public class MemberIdResolver implements HandlerMethodArgumentResolver {
     }
 
     @Override
-    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+    public Member resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
                                   NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
         HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
-        return request.getAttribute("memberId");
+        return memberRepository.findById((Long) request.getAttribute("memberId"))
+                .orElseThrow(() -> new MemberException(NOT_FOUND_MEMBER));
     }
 }
