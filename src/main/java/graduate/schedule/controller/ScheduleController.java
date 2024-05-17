42e00c373f6d9dcea255c2ab2d66b37f8122ed29
ro-el <chung3286@naver.com>
2024-05-17 19:38:46 +0900

    add: create Schedule Controller, Service

---
+++ src/main/java/graduate/schedule/controller/ScheduleController.java
@@ -0,0 +1,19 @@
+package graduate.schedule.controller;
+
+import graduate.schedule.common.response.BaseResponse;
+import graduate.schedule.service.ScheduleService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/schedule")
+public class ScheduleController {
+    private final ScheduleService scheduleService;
+
+
+}
