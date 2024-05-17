cc2bd012b717c4436a5933016b0349ddb0bec75e
ro-el <chung3286@naver.com>
2024-05-17 19:39:25 +0900

    docs: 대타 수락 api note 작성

--- src/main/java/graduate/schedule/controller/ScheduleController.java
+++ src/main/java/graduate/schedule/controller/ScheduleController.java
@@ -15,5 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class ScheduleController {
     private final ScheduleService scheduleService;
 
-
+    /**
+     * @apiNote 대타 수락 api
+     */
+    /*
+    1. 대체 근무 요청자 & 대체 근무자 & 고용인에게 푸시 알림
+    2. StoreMemeberWorkingTime의 member 수정(대체 근무자로) & requestCover를 false로 설정
+    // 수락 누른 사람이 사장인 경우 해당 근무 정보가 삭제
+    * */
 }
